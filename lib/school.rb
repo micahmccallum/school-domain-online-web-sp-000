e2b861c31cd982583cd1267bc6eeeca33e09262e
Micah McCallum <micahmccallum@outlook.com>
2020-03-31 18:37:09 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,10 +10,7 @@ class School
   end
 
   def add_student(student, grade)
-    if roster.has_key?(grade)
-      roster[grade].push(student)
-    else
-      roster[grade] = []
+      roster[grade] ||= []
       roster[grade] << student
     end
   end
